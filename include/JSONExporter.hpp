ef55ada008ea962deb7932309ef21c0f613163f7
Marcin Copik <mcopik@gmail.com>
2018-11-09 03:01:22 +0100

    Group callsites by functions:

--- include/JSONExporter.hpp
+++ include/JSONExporter.hpp
@@ -1,6 +1,8 @@
 #ifndef __JSONEXPORTER_HPP__
 #define __JSONEXPORTER_HPP__
 
+#include <llvm/IR/DebugInfoMetadata.h>
+
 #include <nlohmann/json.hpp>
 
 namespace llvm {
@@ -22,12 +24,15 @@ namespace extrap {
         void export_function(llvm::Function & f, Iter callsite_begin, Iter callsite_end)
         {
             nlohmann::json function = export_function(f);
-            std::vector<nlohmann::json> callsites;
+            //std::vector<nlohmann::json> callsites;
             while(callsite_begin != callsite_end) {
-                callsites.push_back( std::move(export_callsite(*callsite_begin)) );
+                nlohmann::json callsite = export_callsite(*callsite_begin);
+                //group callsites by function
+                std::string function_name = llvm::dyn_cast<llvm::DISubprogram>((*callsite_begin).dbg_loc->getScope())->getName();
+                function["callsites"][function_name].push_back(callsite);
                 ++callsite_begin;
             }
-            function["callsites"] = callsites;
+            //function["callsites"] = callsites;
             out["functions"].push_back(function);
         }
         nlohmann::json export_callsite(CallSite &);
