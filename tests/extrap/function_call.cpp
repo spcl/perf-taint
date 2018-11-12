32a5954ee91599ebd19dd3c73a48e2a3fdd221df
Marcin Copik <mcopik@gmail.com>
2018-11-12 16:42:55 +0100

    Move tests

--- tests/extrap/function_call.cpp
+++ tests/extrap/function_call.cpp
@@ -2,16 +2,19 @@
 #include <cmath>
 #include <cstdlib>
 
-int global = 100;
+#define EXTRAP __attribute__(( annotate("extrap") )) 
+#define EXTRAP __attribute__(( annotate("extrap") )) 
+
+int global EXTRAP = 100;
+
+int f(int x, int y)
+{
+    return 10*x + y/2;
+}
 
 int h(int x)
 {
     return 100 * x * std::log((double)x);
-}
-
-int f(int x, int y)
-{
-    return 10*x + h(y/2);
 }
 
 int g(int x)
@@ -21,25 +24,24 @@ int g(int x)
 
 int i(int x1, int x2, int x3)
 {
-    return f(global, x1) * x1 * x2 + h(x3) * f(2, 5);
+    return 100 * x1 * x2 + x3 * 2;
 }
 
 int main(int argc, char ** argv)
 {
-    int x1 = atoi(argv[1]);
-    int x2 = atoi(argv[2]);
+    int x1 EXTRAP = atoi(argv[1]);
+    int x2 EXTRAP = atoi(argv[2]);
     int y = 2*x1 + 1;
 
     // pass param, pass global
     f(global, x1);
-    f(x2, 100);
     // pass nothing, access global
     g(100);
     // pass nothing, access nothing
     h(x2);
     h(100);
     // pass two args, pass arg, nothing
-    i(y + x2, x1, 15*global);
+    i(y + x2, x1, 15);
 
     return 0;
 }
--- tests/extrap/function_call_nested.cpp
+++ tests/extrap/function_call_nested.cpp
@@ -6,14 +6,14 @@
 
 int global EXTRAP = 100;
 
-int f(int x, int y)
-{
-    return 10*x + y/2;
-}
-
 int h(int x)
 {
     return 100 * x * std::log((double)x);
+}
+
+int f(int x, int y)
+{
+    return 10*x + h(y/2);
 }
 
 int g(int x)
@@ -23,24 +23,25 @@ int g(int x)
 
 int i(int x1, int x2, int x3)
 {
-    return 100 * x1 * x2 + x3 * 2;
+    return f(global, x1) * x1 * x2 + h(x3) * f(2, 5);
 }
 
 int main(int argc, char ** argv)
 {
     int x1 EXTRAP = atoi(argv[1]);
-    int x2 EXTRAP = atoi(argv[2]);
+    int x2 EXTRAP= atoi(argv[2]);
     int y = 2*x1 + 1;
 
     // pass param, pass global
     f(global, x1);
+    f(x2, 100);
     // pass nothing, access global
     g(100);
     // pass nothing, access nothing
     h(x2);
     h(100);
     // pass two args, pass arg, nothing
-    i(y + x2, x1, 15);
+    i(y + x2, x1, 15*global);
 
     return 0;
 }
