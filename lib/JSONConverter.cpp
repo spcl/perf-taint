005d1f312d568ee8c10d864cf62fc20dc0971bbd
Marcin Copik <mcopik@gmail.com>
2019-01-19 00:05:26 +0100

    Group callstacks

--- lib/JSONConverter.cpp
+++ lib/JSONConverter.cpp
@@ -5,6 +5,15 @@
 
 typedef nlohmann::json json_t;
 
+// loop_idx -> loop
+typedef std::map<std::string, std::vector<const json_t*>> set_t;
+
+json_t convert_loop_set(set_t & loop_set)
+{
+    json_t output;
+    return output;
+}
+
 json_t convert(json_t & input)
 {
     json_t output;
@@ -14,8 +23,54 @@ json_t convert(json_t & input)
         "functions_names",
         "parameters"
     };
+    std::vector<std::string> to_copy_local{
+        "file",
+        "line",
+        "dbg_name"
+    };
     for(const auto & key : to_copy) {
         output[key] = std::move(input[key]);
+    }
+
+    json_t & functions = input["functions"];
+    json_t & functions_output = output["functions"];
+    for(auto it = functions.begin(), end = functions.end(); it != end; ++it) {
+
+        const json_t & loops = it.value()["loops"];
+        // callstack -> list of loops
+        std::map<json_t, set_t> aggregated_callstacks;
+        for(auto it = loops.begin(), end = loops.end(); it != end; ++it) {
+            std::string loop_idx = it.key();
+            const auto & loop_instances = it.value();
+            for(auto & loop_instance : loop_instances) {
+                const auto & callstacks = loop_instance["callstacks"];
+                for(const auto & callstack : callstacks) {
+                    //std::cout << callstack << '\n';
+                    auto find = aggregated_callstacks.find(callstack);
+                    if(find != aggregated_callstacks.end()) {
+                        (*find).second[loop_idx].push_back(&loop_instance["instance"]);
+                    } else {
+                        aggregated_callstacks[callstack][loop_idx] =
+                            std::vector<const json_t*>{1, &loop_instance["instance"]};
+                    }
+                }
+            }
+        }
+
+        json_t new_loops;
+        for(auto & callstack : aggregated_callstacks) {
+            json_t instance;
+            instance["callstack"] = callstack.first;
+            instance["data"] = convert_loop_set(callstack.second);
+            new_loops.push_back(std::move(instance));
+        }
+
+        json_t function;
+        function["loops"] = std::move(new_loops);
+        for(const auto & key : to_copy_local) {
+            function[key] = std::move(it.value()[key]);
+        }
+        functions_output[it.key()] = std::move(function);
     }
 
     return output;
