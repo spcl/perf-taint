e0bd36bd8119139a2a52c1581b972d69714a5365
Marcin Copik <mcopik@gmail.com>
2019-01-20 16:53:53 +0100

    Improve dependency generation

--- lib/JSONConverter.cpp
+++ lib/JSONConverter.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <set>
 #include <nlohmann/json.hpp>
 
 typedef nlohmann::json json_t;
@@ -20,10 +21,10 @@ json_t convert_params(const json_t & params)
         } else
             ops.push_back(param);
     }
-    if(ops.size() == 1)
-        return ops;
-    else
-        return output;
+    //if(ops.size() == 1)
+    //    return ops[0];
+    //else
+    return output;
 }
 
 json_t convert_loop(const json_t & loop)
@@ -100,14 +101,132 @@ json_t convert_loop_set(set_t & loop_set)
     json_t & deps = output["operands"];
     for(auto & val : loop_set)
     {
-        // TODO: here we only take a single instance of 
+        // TODO : change when we get sequence of loops comitted at instance
         json_t dep;
-        for(auto v : val.second)
-            dep.push_back(convert_loop(**v));
+        for(auto v : val.second) {
+            dep.push_back(convert_loop(*v));
+        }
         deps.push_back(dep);
     }
 
     return output;
+}
+
+uint32_t param_to_int(const json_t & op, const json_t & params)
+{
+    uint32_t res = 0;
+    for(auto & v: op) {
+        size_t i = 0;
+        for(; i < params.size(); ++i)
+            if(params[i] == v)
+                break;
+        res |= (1 << i);
+    }
+    return res;
+}
+
+std::vector<uint32_t> parse(const json_t & op, const json_t & all_params)
+{
+    // now we process each instance per callstack
+    //if(op.is_array()) {
+    //    std::cout << "Process param: " << op << ' ' << param_to_int(op, all_params) << '\n';
+    //    return std::vector<uint32_t>(1, param_to_int(op, all_params));
+    //} else
+    //if(op.is_array()) {
+    //    std::cout << "Process param: " << op << ' ' << param_to_int(op, all_params) << '\n';
+    //    return std::vector<uint32_t>(1, param_to_int(op, all_params));
+    //} else
+    if(op.is_array()) {
+        return parse(op[0], all_params);
+    } else if(op["dependency"] == "additive") {
+        const json_t ops = op["operands"];
+        std::vector<uint32_t> params;
+        for(const auto & v : ops) {
+            std::cout << "Process additive arg: " << v << '\n';
+            auto res = parse(v, all_params);
+            std::cout << "Process additive arg: " << v << " results: ";
+            for(uint32_t val : res) {
+                std::cout << val << ' ';
+                params.push_back(val);
+            }
+            std::cout << '\n';
+        }
+        std::cout << "Process additive: " << ops << ' ' << params.size() << '\n';
+        return params;
+    } else if(op["dependency"] == "multiplicative") {
+        const json_t ops = op["operands"];
+        std::vector<uint32_t> params = parse(ops[0], all_params);
+        std::cout << "Process multiplicative arg: " << ops[0]  << " begin: ";
+        for(auto v : params)
+            std::cout << v << ' ';
+        std::cout << '\n';
+        // TODO: remove duplicates
+        for(int i = 1; i < ops.size(); ++i) {
+           auto res = parse(ops[i], all_params);
+           std::vector<uint32_t> new_params;
+            std::cout << "Process multiplicative arg: " << ops[i]  << " results: ";
+            for(int i = 0; i < params.size(); ++i) {
+               for(int j = 0; j < res.size(); ++j) {
+                    uint32_t result = params[i] | res[j];
+                    std::cout << result << ' ';
+                    new_params.push_back(result);
+               }
+            }
+            std::cout << '\n';
+            // if there is nothing beneath, don't override old results
+            if(res.size() != 0)
+                params = std::move(new_params);
+        }
+        std::cout << "Process mul: " << ops << ' ' << params.size() << '\n';
+        return params;
+    } else if(op["dependency"] == "unknown") {
+        uint32_t res = 0;
+        std::cout << "Process unknown: " << op["operands"] << '\n';
+        for(const auto & v : op["operands"])
+            res |= param_to_int(v, all_params);
+        std::cout << "Process unknown: " << op["operands"] << ' ' << res << '\n';
+        return std::vector<uint32_t>{res};
+    } else {
+        assert(false);
+    }
+}
+
+void get_deps(json_t & loop, const json_t & params)
+{
+    // pessimistic implementation - each unknown is multiplication
+    // TODO : change when we get sequence of loops comitted at instance
+    json_t & ops = loop["operands"];
+    json_t & deps = loop["deps"];
+    std::set<uint32_t> dependencies;
+    json_t & not_found_params = loop["not_found_params"];
+    uint32_t aggregation = 0;
+    // TODO: nested array, shouldn't be here
+    // right now we have have a list of loops and for each one list of instances
+    // this should be a list of instances where each one is additive instance
+    for(const auto & op_array : ops)
+    {
+        for(const auto & op : op_array)
+        {
+            std::cout << "Op: " << op << '\n';
+            std::vector<uint32_t> results = parse(op, params);
+            for(uint32_t v : results)
+            {
+                aggregation |= v;
+                dependencies.insert(v);
+            }
+        }
+    }
+    for(int i = 0; i < params.size(); ++i)
+        if(!(aggregation & (1 << i)))
+           not_found_params.push_back(params[i]);
+    for(auto v : dependencies)
+    {
+        json_t dependency;
+        for(int i = 0; i < params.size(); ++i)
+            if(v & (1 << i))
+                dependency.push_back(params[i]);
+        deps.push_back( std::move(dependency) );
+    }
 }
 
 json_t convert(json_t & input)
@@ -132,6 +251,7 @@ json_t convert(json_t & input)
     json_t & functions_output = output["functions"];
     for(auto it = functions.begin(), end = functions.end(); it != end; ++it) {
 
+        std::cout << "Name: " << it.key() << '\n';
         const json_t & loops = it.value()["loops"];
         // callstack -> list of loops
         std::map<json_t, set_t> aggregated_callstacks;
@@ -157,7 +277,9 @@ json_t convert(json_t & input)
         for(auto & callstack : aggregated_callstacks) {
             json_t instance;
             instance["callstack"] = callstack.first;
+            //std::cout << "Start data: " << callstack.second << '\n';
             instance["data"] = convert_loop_set(callstack.second);
+            get_deps(instance["data"], output["parameters"]);
             new_loops.push_back(std::move(instance));
         }
 
