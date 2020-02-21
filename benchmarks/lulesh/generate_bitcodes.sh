d1a49e3bc6c77a7305e6a24ea8932666f93713c7
Marcin Copik <mcopik@gmail.com>
2020-02-21 15:42:28 +0100

    Add Lulesh bitcode generation

--- benchmarks/lulesh/generate_bitcodes.sh
+++ benchmarks/lulesh/generate_bitcodes.sh
@@ -1,4 +1,19 @@
-#!/usr/bin/bash
+#!/bin/bash
 
+SOURCE_DIRECTORY=$1
+BUILD_DIRECTORY=$2
+
+pushd ${SOURCE_DIRECTORY}/benchmarks/lulesh > /dev/null
 git submodule update --recursive --init LULESH
-cd LULESH && git checkout a201764b50632a831e2b57072c4b1d933ef9acb4
+cd LULESH\
+  && git checkout a201764b50632a831e2b57072c4b1d933ef9acb4\
+  && git checkout -- .\
+  && git apply ../lulesh.patch
+
+# Build with MPI
+make CXX=${BUILD_DIRECTORY}/bin/clang++ CXXFLAGS="-g -I. -Wall -DUSE_MPI=1" LDFLAGS="-g" all
+llvm-link *.bc -o lulesh_mpi.bc
+mv lulesh_mpi.bc ..
+make clean && rm *.bc
+
+popd > /dev/null
