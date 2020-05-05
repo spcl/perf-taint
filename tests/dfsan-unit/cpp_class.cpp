175c1971ebdff51067228d62cac7c0b9dd1b2eeb
Marcin Copik <mcopik@gmail.com>
2020-05-05 18:05:58 +0200

    Two additional dfsan-only tests

--- tests/dfsan-unit/cpp_class.cpp
+++ tests/dfsan-unit/cpp_class.cpp
@@ -1,3 +1,10 @@
+// RUN: %clangxx %cxx_flags %s -emit-llvm -o %t1.bc
+// RUN: %opt %opt_flags < %t1.bc 2> /dev/null > %t1.tainted.bc
+// RUN: %llc %llc_flags < %t1.tainted.bc > %t1.tainted.o
+// RUN: %clangxx %link_flags %t1.tainted.o -o %t1.exe
+// RUN: %execparams %t1.exe 10 10 10 > %t1.json
+// RUN: diff -w %s.json %t1.json
+
 #include <cstdint>
 #include <cstdlib>
 
