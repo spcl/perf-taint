c43dbd68f0103736904bae38df0e4e458986975c
Marcin Copik <mcopik@gmail.com>
2020-05-05 18:00:27 +0200

    Update MPI tests

--- tests/dfsan-unit/mpi/taint_collectives.cpp
+++ tests/dfsan-unit/mpi/taint_collectives.cpp
@@ -1,3 +1,20 @@
+// RUN: %clangxx %cxx_flags %mpi_flags %s -emit-llvm -o %t1.bc
+// RUN: %opt %opt_flags %opt_mpi -extrap-extractor-out-name=%t3 < %t1.bc \
+// RUN:     2> /dev/null > %t1.tainted.bc
+// RUN: %llc %llc_flags < %t1.tainted.bc > %t1.tainted.o
+// RUN: %clangxx %link_flags %t1.tainted.o -o %t1.exe
+// RUN: %execparams mpiexec -n 2 %t1.exe
+// RUN: diff -w %s.json %t3_0.json
+// RUN: diff -w %s.json %t3_1.json
+
+// RUN: %opt %opt_flags %opt_mpi %opt_cfsan -extrap-extractor-out-name=%t4 \
+// RUN:     < %t1.bc 2> /dev/null > %t2.tainted.bc
+// RUN: %llc %llc_flags < %t2.tainted.bc > %t2.tainted.o
+// RUN: %clangxx %link_flags %t2.tainted.o -o %t2.exe
+// RUN: %execparams mpiexec -n 2 %t2.exe
+// RUN: diff -w %s.json %t4_0.json
+// RUN: diff -w %s.json %t4_1.json
+
 #include <stdio.h>
 #include <stdlib.h>
 #define OMPI_SKIP_MPICXX
