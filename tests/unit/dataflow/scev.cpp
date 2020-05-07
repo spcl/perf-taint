5e2ec1177c40c068dee55e1c103c9b4034d636d2
Marcin Copik <mcopik@gmail.com>
2020-05-07 21:21:48 +0200

    extend test with more variants

--- tests/unit/dataflow/scev.cpp
+++ tests/unit/dataflow/scev.cpp
@@ -38,6 +38,40 @@ int mixed_loop(int x1, int x2)
   return tmp + x1 + x2;
 }
 
+// constant (compile-time)
+// instrumented (tainted) three variants
+int nonconstant_loop_variants(int x1, int x2)
+{
+  int tmp = 0;
+  for(int i = 10; i < 93; i += 2)
+      tmp += i;
+  for(int i = 0; i < x1; ++i)
+      tmp += i;
+  for(int i = x1; i < 100; ++i)
+      tmp += i;
+  for(int i = 0; i < 100; i += x1)
+      tmp += i;
+  return tmp;
+}
+
+// constant (compile-time)
+// instrumented (tainted)
+// instrumented (constant)
+// instrumented (tainted)
+int mixed_loop_large(int x1, int x2, int x3)
+{
+  int tmp = 0;
+  for(int i = 10; i < 93; i += 2)
+      tmp += i;
+  for(int i = 0; i < x1; ++i)
+      tmp += i;
+  for(int i = x1; i < x3; ++i)
+      tmp += i;
+  for(int i = x1; i < x2; i++)
+      tmp += i;
+  return tmp + x1 + x2;
+}
+
 int main(int argc, char ** argv)
 {
   int x1 = atoi(argv[1]);
@@ -46,7 +80,9 @@ int main(int argc, char ** argv)
   empty_function(x1, x2);
   constant_loop(x1, x2);
   nonconstant_loop(x1, x2);
+  nonconstant_loop_variants(x1, x2);
   mixed_loop(x1, x2);
+  mixed_loop_large(x1, x2, 100);
 
   return 0;
 }
