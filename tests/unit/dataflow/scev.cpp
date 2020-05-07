056a92ad9c886449bad4b513cf1d2e21c3e10f6f
Marcin Copik <mcopik@gmail.com>
2020-05-07 22:04:49 +0200

    Update SCEV tests

--- tests/unit/dataflow/scev.cpp
+++ tests/unit/dataflow/scev.cpp
@@ -3,7 +3,14 @@
 // RUN: %t1.bc 2> /dev/null > %t1.tainted.bc
 // RUN: diff -w %s.pass.json %t1.scev.json
 
+// RUN: %llc %llc_flags < %t1.tainted.bc > %t1.tainted.o
+// RUN: %clangxx %link_flags %t1.tainted.o -o %t1.exe
+// RUN: %execparams %t1.exe 10 10 > %t1.json
+// RUN: diff -w %s.json %t1.json
+
 #include <cstdlib>
+
+#include <perf-taint/PerfTaint.hpp>
 
 // This test verifies that using LLVM SCEV helps to prune some loops.
 
@@ -74,8 +81,10 @@ int mixed_loop_large(int x1, int x2, int x3)
 
 int main(int argc, char ** argv)
 {
-  int x1 = atoi(argv[1]);
-  int x2 = atoi(argv[2]);
+  int x1 EXTRAP = atoi(argv[1]);
+  int x2 EXTRAP = 2*atoi(argv[2]);
+  register_variable(&x1, "x1");
+  register_variable(&x2, "x2");
 
   empty_function(x1, x2);
   constant_loop(x1, x2);
