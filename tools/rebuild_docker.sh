7772e2706222426cb48cb7d068eb39d68cc34abc
Marcin Copik <mcopik@gmail.com>
2021-07-15 03:33:20 +0200

    Update Docker images

--- tools/rebuild_docker.sh
+++ tools/rebuild_docker.sh
@@ -4,3 +4,6 @@
 docker build --build-arg BASE=dfsan-9.0 -f docker/Dockerfile.base -t spcleth/perf-taint:base-dfsan-9.0 .
 docker build --build-arg BASE=cfsan-9.0 -f docker/Dockerfile.base -t spcleth/perf-taint:base-cfsan-9.0 .
 
+# Build perf-taint
+docker build -f docker/Dockerfile.perf-taint -t spcleth/perf-taint:0.1 .
+
